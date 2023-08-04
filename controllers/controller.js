f4d5740804c3886152920c8e5cc8212b4f5238fa
Yanako Lajara <yanakolajara@pursuit.org>
2023-08-04 12:52:42 -0400

    recipes routes created

--- controllers/controller.js
+++ controllers/controller.js
@@ -5,3 +5,31 @@ const {
     getAllRecipes,
     getRecipeById
 } = require('../queries/recipes')
+
+router.get('/', async(req,res) => {
+    try {
+        const allRecipes = await getAllRecipes();
+        if(allRecipes[0]){
+            res.json(allRecipes);
+        }else{
+            res.status(500).json({error: "Server error"});
+        }
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+})
+
+router.get('/:id', async(req,res) => {
+    try {
+        const recipeById = await getRecipeById(req.params.id);
+        if(!!recipeById && recipeById.length() > 0){
+            res.json(recipeById);
+        }else{
+            res.status(404).json({error: "Not found"});
+        }
+    } catch (error) {
+        res.status(500).json({error: error});
+    }
+})
+
+module.exports = router;
