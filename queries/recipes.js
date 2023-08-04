b9672b80346eb610ed20a9687047b969afad24c1
Yanako Lajara <yanakolajara@pursuit.org>
2023-08-04 12:41:10 -0400

    Recipes fetch calls

---
+++ queries/recipes.js
@@ -0,0 +1,23 @@
+const db = require('../db/dbConfig');
+
+const getAllRecipes = async() => {
+    try {
+        const fetch = db.any("SELECT * FROM recipes");
+        return fetch;
+    } catch (error) {
+        return error;
+    }
+}
+
+const getRecipeById = async(id) => {
+    try {
+        const fetch = db.any(`SELECT * FROM recipes WHERE id=$1`,[id]);
+        return fetch;
+    } catch (error) {
+        return error;
+    }
+}
+
+module.exports = {
+
+}
