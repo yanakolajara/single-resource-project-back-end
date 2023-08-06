3287222c4184bafbb070d9b3f6e7a1c3ad3f8a47
Yanako Lajara <yanakolajara@pursuit.org>
2023-08-06 17:16:17 -0400

    Correction to variables' names

--- queries/recipes.js
+++ queries/recipes.js
@@ -2,8 +2,8 @@ const db = require('../db/dbConfig');
 
 const getAllRecipes = async() => {
     try {
-        const fetch = await db.any("SELECT * FROM recipes");
-        return fetch;
+        const allRecipes = await db.any("SELECT * FROM recipes");
+        return allRecipes;
     } catch (error) {
         return error;
     }
@@ -11,19 +11,19 @@ const getAllRecipes = async() => {
 
 const getRecipeById = async(id) => {
     try {
-        const fetch = await db.any(`SELECT * FROM recipes WHERE id=$1`,[id]);
-        return fetch;
+        const recipeById = await db.any(`SELECT * FROM recipes WHERE id=$1`,[id]);
+        return recipeById;
     } catch (error) {
         return error;
     }
 }
 
-const createRecipe = async(name,photo,type,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime) => {
+const createNewRecipe = async(name,photo,type,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime) => {
     try {
-        const fetch = await db.any('INSERT INTO recipes (name, photo, type, cuisine,  is_vegan, difficulty, ingredients, description) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
+        const newRecipe = await db.any('INSERT INTO recipes (name, photo, type, cuisine,  is_vegan, difficulty, ingredients, description) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9) RETURNING *',
         [name,photo,type,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime]
         );
-        return fetch;
+        return newRecipe;
     } catch (error) {
         return error;
     }
