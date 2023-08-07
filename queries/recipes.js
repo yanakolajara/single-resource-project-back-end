4871258b1973e79e96e5b7324c18faada41a54f5
Yanako Lajara <yanakolajara@pursuit.org>
2023-08-07 10:26:39 -0400

    Deleted UPDATE/DELETE controllers and queries

--- queries/recipes.js
+++ queries/recipes.js
@@ -18,31 +18,7 @@ const getRecipeById = async(id) => {
     }
 }
 
-const createNewRecipe = async(name,photo,type,cuisine,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime) => {
-    try {
-        const newRecipe = await db.any('INSERT INTO recipes (name,photo,type,cuisine,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *',
-        [name,photo,type,cuisine,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime]
-        );
-        return newRecipe;
-    } catch (error) {
-        return error;
-    }
-}
-
-const updateRecipe = async(name,photo,type,cuisine,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime,id) => {
-    try {
-        const updatedRecipe = await db.any('UPDATE recipes SET name = $1, photo = $2, type = $3, cuisine = $4, is_healthy = $5, is_vegan = $6, difficulty = $7, ingredient = $8, description = $9, cookingtime = $10 WHERE id = $11 RETURNING *',
-        [name,photo,type,cuisine,is_healthy,is_vegan,difficulty,ingredient,description,cookingtime,id]
-        );
-        return updatedRecipe;
-    } catch (error) {
-        return error;
-    }
-}
-
 module.exports = {
     getAllRecipes,
     getRecipeById,
-    createNewRecipe,
-    updateRecipe
 }
