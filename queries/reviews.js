453a0b33e946aff9b1005572c1b07fa6cc987884
Amina-Moufakkir <aminamouf22@gmail.com>
2023-08-08 11:27:09 -0400

    fix reviews getReviewsOfRecipe

--- queries/reviews.js
+++ queries/reviews.js
@@ -1,24 +1,27 @@
 const db = require('../db/dbConfig');
 
-const getAllReviews = async() => {
-    try {
-        const allReviews = await db.any('SELECT * FROM reviews');
-        return allReviews;
-    } catch (error) {
-        return error;
-    }
-}
+const getAllReviews = async () => {
+  try {
+    const allReviews = await db.any('SELECT * FROM reviews');
+    return allReviews;
+  } catch (error) {
+    return error;
+  }
+};
 
-const getReviewsOfRecipe = async(recipeId) => {
-    try {
-        const recipeReviews = await db.any('SELECT * FROM reviews WHERE recipes_id = $1',[recipeId]);
-        return recipeReviews
-    } catch (error) {
-        return error
-    }
-}
+const getReviewsOfRecipe = async (recipeId) => {
+  try {
+    const recipeReviews = await db.any(
+      'SELECT * FROM reviews WHERE recipe_id = $1',
+      [recipeId]
+    );
+    return recipeReviews;
+  } catch (error) {
+    return error;
+  }
+};
 
 module.exports = {
-    getAllReviews,
-    getReviewsOfRecipe
-}
+  getAllReviews,
+  getReviewsOfRecipe,
+};
