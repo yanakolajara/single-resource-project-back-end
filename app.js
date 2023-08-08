38fa3dc75e4a0a6309487169ed2d79c84108d808
Amina-Moufakkir <aminamouf22@gmail.com>
2023-08-07 20:25:50 -0400

    reviewController not found error

--- app.js
+++ app.js
@@ -3,23 +3,21 @@ const morgan = require('morgan');
 const cors = require('cors');
 
 const app = express();
-const recipes = require('./controllers/recipesController')
-const reviews = require('./controllers/reviewsController')
-
+const recipes = require('./controllers/recipesController');
+// const reviews = require('./controllers/reviewsController');
 
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-app.use('/recipes', recipes)
-app.use('/reviews', reviews)
-
+app.use('/recipes', recipes);
+// app.use('/reviews', reviews);
 
 app.get('/', (req, res) => {
   res.send('Hello, world!');
 });
 
-app.get('*', (req,res) => {
-  res.status(404).send('Not found')
-})
+app.get('*', (req, res) => {
+  res.status(404).send('Not found');
+});
 
 module.exports = app;
